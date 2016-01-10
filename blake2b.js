504056471c622ba5f67f2b740fcfbcbfdc19a6a1
DC <dcposch@dcpos.ch>
2016-01-10 01:04:01 -0800

    Standardize, use tape for tests

--- blake2b.js
+++ blake2b.js
@@ -4,7 +4,7 @@
 
 // Cyclic 64-bit right rotation. x0 is low 32 bits, x1 is high 32 bits
 // Returns [low 32 bit output, high 32 bit output]
-function ROTR64(x, y) {
+function ROTR64 (x, y) {
   var x0 = x[0]
   var x1 = x[1]
   // uint64 rotation is NOT efficient when all you have is float64 :(
@@ -20,64 +20,12 @@ function ROTR64(x, y) {
   return [o0, o1]
 }
 
-// For debugging: prints out a uint64 represented as an array of two int32s
-// Examples:
-//  HEX64([0x4030201, 0x8070605]) = '0x0807060504030201'
-//  HEX64(ROT64(0x4030201, 0x8070605, 16)) = '0x0201080706050403'
-function HEX64(arr) {
-  var s0 = (arr[0] >= 0 ? arr[0] : 0x100000000 + arr[0]).toString(16)
-  var s1 = (arr[1] >= 0 ? arr[1] : 0x100000000 + arr[1]).toString(16)
-  return '0x' +
-    new Array(8 - s1.length+1).join('0') + s1 +
-    new Array(8 - s0.length+1).join('0') + s0
-}
-
-// For debugging: prints out hash state in the same format as the RFC
-// sample computation
-function DebugPrint(label, arr) {
-  var msg = '\n' + label + ' = '
-  for (var i = 0; i < arr.length; i++) {
-    msg += HEX64(arr[i]).substring(2).toUpperCase()
-    if (i % 3 === 2) {
-      msg += '\n' + new Array(label.length + 4).join(' ')
-    } else if (i < arr.length - 1) {
-      msg += ' '
-    }
-  }
-  console.log(msg)
-}
-
 // Little-endian byte access
-function B2B_GET32(arr, i) {
+function B2B_GET32 (arr, i) {
   return arr[i] ^
-    (arr[i+1]<<8) ^
-    (arr[i+2]<<16) ^
-    (arr[i+3]<<24)
-}
-
- //Adds two little endian uint64s, arr[a,a+1], arr[b,b+1], stores in arr[x,x+1]
- //Expects arr to be a Uint32Array
-function AADD64(arr, a, b, x) {
-  var o0 = arr[a] + arr[b]
-  var o1 = arr[a+1] + arr[b+1]
-  if (arr[a] + arr[b] >= 0x100000000) {
-    o1++
-  }
-  arr[x] = o0
-  arr[x+1] = o1
-}
-
- //Adds a small integer b to a little endian uint64 arr[a,a+1], stores in
- //arr[x,x+1]
- //Expects arr to be a Uint32Array
-function AADD64S(arr, a, b, x) {
-  var o0 = arr[a] + b
-  var o1 = arr[a+1]
-  if (arr[a] + b >= 0x100000000) {
-    o1++
-  }
-  arr[x] = o0
-  arr[x+1] = o1
+  (arr[i + 1] << 8) ^
+  (arr[i + 2] << 16) ^
+  (arr[i + 3] << 24)
 }
 
 // Javascript is just disgusting...
@@ -87,12 +35,12 @@ function AADD64S(arr, a, b, x) {
 // console.log(arr[0] | 0) // prints -1
 //
 // So when working with 64-bit #s, the two 32-bit halves are signed int32s
-function ADD64(a, b) {
+function ADD64 (a, b) {
   var o0 = a[0] + b[0]
-  if(a[0] < 0) {
+  if (a[0] < 0) {
     o0 += 0x100000000
   }
-  if(b[0] < 0) {
+  if (b[0] < 0) {
     o0 += 0x100000000
   }
   var o1 = a[1] + b[1]
@@ -102,14 +50,14 @@ function ADD64(a, b) {
   return new Uint32Array([o0, o1])
 }
 
-function XOR64(a, b) {
+function XOR64 (a, b) {
   var o0 = a[0] ^ b[0]
   var o1 = a[1] ^ b[1]
   return new Uint32Array([o0, o1])
 }
 
 // G Mixing function.
-function B2B_G(v, a, b, c, d, x, y) {
+function B2B_G (v, a, b, c, d, x, y) {
   v[a] = ADD64(ADD64(v[a], v[b]), x)
   v[d] = ROTR64(XOR64(v[d], v[a]), 32)
   v[c] = ADD64(v[c], v[d])
@@ -129,8 +77,8 @@ var BLAKE2B_IV32 = new Uint32Array([
 ])
 
 var BLAKE2B_IV64 = []
-for(var i = 0; i < BLAKE2B_IV32.length; i += 2) {
-  BLAKE2B_IV64.push([BLAKE2B_IV32[i+1], BLAKE2B_IV32[i]])
+for (var i = 0; i < BLAKE2B_IV32.length; i += 2) {
+  BLAKE2B_IV64.push([BLAKE2B_IV32[i + 1], BLAKE2B_IV32[i]])
 }
 
 var SIGMA8 = new Uint8Array([
@@ -145,12 +93,11 @@ var SIGMA8 = new Uint8Array([
   6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5,
   10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0,
   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-  14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3,
+  14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3
 ])
 
-
 // Compression function. 'last' flag indicates last block.
-function blake2b_compress(ctx, last) {
+function blake2b_compress (ctx, last) {
   var i = 0
   var v = new Array(16)
   var m = new Array(16)
@@ -158,13 +105,13 @@ function blake2b_compress(ctx, last) {
   // init work variables
   for (i = 0; i < 8; i++) {
     v[i] = ctx.h[i]
-    v[i + 8] = BLAKE2B_IV64[i];
+    v[i + 8] = BLAKE2B_IV64[i]
   }
 
   // low 64 bits of offset
   v[12] = XOR64(v[12], [ctx.t, 0])
   // high 64 bits
-  //v[13] = XOR64(v[13], ctx.t[1])
+  // v[13] = XOR64(v[13], ctx.t[1])
   // last block flag set ?
   if (last) {
     v[14] = [~v[14][0], ~v[14][1]]
@@ -179,32 +126,26 @@ function blake2b_compress(ctx, last) {
   }
 
   // twelve rounds of mixing
-  // uncomment the DebugPrint calls to log the computation
-  // and match the RFC sample documentation
-  // DebugPrint('          m[16]', m)
   for (i = 0; i < 12; i++) {
-    // DebugPrint('   (i='+(i<10?' ':'')+i+') v[16]', v)
-    B2B_G(v, 0, 4,  8, 12, m[SIGMA8[i*16 + 0]], m[SIGMA8[i*16 + 1]])
-    B2B_G(v, 1, 5,  9, 13, m[SIGMA8[i*16 + 2]], m[SIGMA8[i*16 + 3]])
-    B2B_G(v, 2, 6, 10, 14, m[SIGMA8[i*16 + 4]], m[SIGMA8[i*16 + 5]])
-    B2B_G(v, 3, 7, 11, 15, m[SIGMA8[i*16 + 6]], m[SIGMA8[i*16 + 7]])
-    B2B_G(v, 0, 5, 10, 15, m[SIGMA8[i*16 + 8]], m[SIGMA8[i*16 + 9]])
-    B2B_G(v, 1, 6, 11, 12, m[SIGMA8[i*16 +10]], m[SIGMA8[i*16 +11]])
-    B2B_G(v, 2, 7,  8, 13, m[SIGMA8[i*16 +12]], m[SIGMA8[i*16 +13]])
-    B2B_G(v, 3, 4,  9, 14, m[SIGMA8[i*16 +14]], m[SIGMA8[i*16 +15]])
+    B2B_G(v, 0, 4, 8, 12, m[SIGMA8[i * 16 + 0]], m[SIGMA8[i * 16 + 1]])
+    B2B_G(v, 1, 5, 9, 13, m[SIGMA8[i * 16 + 2]], m[SIGMA8[i * 16 + 3]])
+    B2B_G(v, 2, 6, 10, 14, m[SIGMA8[i * 16 + 4]], m[SIGMA8[i * 16 + 5]])
+    B2B_G(v, 3, 7, 11, 15, m[SIGMA8[i * 16 + 6]], m[SIGMA8[i * 16 + 7]])
+    B2B_G(v, 0, 5, 10, 15, m[SIGMA8[i * 16 + 8]], m[SIGMA8[i * 16 + 9]])
+    B2B_G(v, 1, 6, 11, 12, m[SIGMA8[i * 16 + 10]], m[SIGMA8[i * 16 + 11]])
+    B2B_G(v, 2, 7, 8, 13, m[SIGMA8[i * 16 + 12]], m[SIGMA8[i * 16 + 13]])
+    B2B_G(v, 3, 4, 9, 14, m[SIGMA8[i * 16 + 14]], m[SIGMA8[i * 16 + 15]])
   }
-  // DebugPrint('   (i=12) v[16]', v)
 
   for (i = 0; i < 8; i++) {
     ctx.h[i] = XOR64(ctx.h[i], XOR64(v[i], v[i + 8]))
   }
-  // DebugPrint('h[8]', ctx.h)
 }
 
 // Initialize the hashing context with optional key 'key'
 // Returns a newly created context
-function blake2b_init(outlen, key) {
-  if (outlen == 0 || outlen > 64) {
+function blake2b_init (outlen, key) {
+  if (outlen === 0 || outlen > 64) {
     throw new Error('Illegal output length, expected 0 < length <= 64')
   }
   if (key && key.length > 64) {
@@ -238,23 +179,23 @@ function blake2b_init(outlen, key) {
 }
 
 // Add 'in' into the hash. Expects a Uint8Array
-function blake2b_update(ctx, input) {
+function blake2b_update (ctx, input) {
   for (var i = 0; i < input.length; i++) {
-    if (ctx.c === 128) {             // buffer full ?
-        ctx.t += ctx.c               // add counters
-        blake2b_compress(ctx, false) // compress (not last)
-        ctx.c = 0                    // counter to zero
+    if (ctx.c === 128) { // buffer full ?
+      ctx.t += ctx.c // add counters
+      blake2b_compress(ctx, false) // compress (not last)
+      ctx.c = 0 // counter to zero
     }
-    ctx.b[ctx.c++] = input[i];
+    ctx.b[ctx.c++] = input[i]
   }
 }
 
 // Generate the message digest.
 // Returns a Uint8Array
-function blake2b_final(ctx) {
-  ctx.t += ctx.c              // mark last block offset
+function blake2b_final (ctx) {
+  ctx.t += ctx.c // mark last block offset
 
-  while (ctx.c < 128) {       // fill up with zeros
+  while (ctx.c < 128) { // fill up with zeros
     ctx.b[ctx.c++] = 0
   }
   blake2b_compress(ctx, true) // final block flag = 1
@@ -262,7 +203,7 @@ function blake2b_final(ctx) {
   // little endian convert and store
   var out = new Uint8Array(ctx.outlen)
   for (var i = 0; i < ctx.outlen; i++) {
-    out[i] = (ctx.h[i >> 3][(i >> 2) & 1] >> (8 * (i & 3))) & 0xFF;
+    out[i] = (ctx.h[i >> 3][(i >> 2) & 1] >> (8 * (i & 3))) & 0xFF
   }
   return out
 }
@@ -275,10 +216,10 @@ function blake2b_final(ctx) {
 // - input - the input bytes, as a Uint8Array or ASCII string
 // - key - optional key, either a 32 or 64-byte Uint8Array
 // - outlen - optional output length in bytes, default 64
-function blake2b(input, key, outlen) {
+function blake2b (input, key, outlen) {
   // preprocess inputs
   outlen = outlen || 64
-  if (typeof(input) === 'string') {
+  if (typeof (input) === 'string') {
     var str = input
     input = new Uint8Array(str.length)
     for (var i = 0; i < str.length; i++) {
@@ -302,14 +243,20 @@ function blake2b(input, key, outlen) {
 // Parameters:
 // - input - the input bytes, as a Uint8Array or ASCII string
 // - outlen - optional output length in bytes, default 64
-function blake2bHex(input, outlen) {
+function blake2bHex (input, outlen) {
   var output = blake2b(input, null, outlen)
-  return Array.prototype.map.call(output, function(n) {
+  return Array.prototype.map.call(output, function (n) {
     return (n < 16 ? '0' : '') + n.toString(16)
   }).join('')
 }
 
 module.exports = {
   blake2b: blake2b,
-  blake2bHex: blake2bHex
+  blake2bHex: blake2bHex,
+  // visible for testing
+  test: {
+    ROTR64: ROTR64,
+    ADD64: ADD64,
+    XOR64: XOR64
+  }
 }
--- test_blake2b.js
+++ test_blake2b.js
@@ -1,76 +1,64 @@
+var test = require('tape')
+var blake2b = require('./blake2b')
 
-function testROTR64() {
-  assertEquals('0x0807060504030201', HEX64(ROTR64([0x4030201, 0x8070605], 0)))
-  assertEquals('0x0201080706050403', HEX64(ROTR64([0x4030201, 0x8070605], 16)))
-  assertEquals('0x4030201080706050', HEX64(ROTR64([0x4030201, 0x8070605], 28)))
-  assertEquals('0x1080706050403020', HEX64(ROTR64([0x4030201, 0x8070605], 4)))
-  assertEquals('0x0403020108070605', HEX64(ROTR64([0x4030201, 0x8070605], 32)))
-  assertEquals('0x8070605040302010', HEX64(ROTR64([0x4030201, 0x8070605], 60)))
-  assertEquals('0x7772fc2886a76c5f', HEX64(ROTR64([0x6c5f7772, 0xfc2886a7], 16)))
-}
+var blake2bHex = blake2b.blake2bHex
+var ROTR64 = blake2b.test.ROTR64
+var ADD64 = blake2b.test.ADD64
+var XOR64 = blake2b.test.XOR64
 
-function testADD64() {
-  assertEquals('0x0000000200000000', HEX64(ADD64([0xffffffff, 1], [1, 0])))
-  assertEquals('0x00000002fffffffe', HEX64(ADD64([0xffffffff, 1], [0xffffffff, 0])))
-  assertEquals('0x76eb1310ddd333a0', HEX64(ADD64([0xF3BCC908, 0x6A09E667], [0xEA166A98, 0x0CE12CA8])))
-  assertEquals('0x76eb1310ddd333a0', HEX64(ADD64([-205731576, 1779033703], [-367629672, 216083624])))
-}
+test('ROTR64', function (t) {
+  t.equal('0x0807060504030201', HEX64(ROTR64([0x4030201, 0x8070605], 0)))
+  t.equal('0x0201080706050403', HEX64(ROTR64([0x4030201, 0x8070605], 16)))
+  t.equal('0x4030201080706050', HEX64(ROTR64([0x4030201, 0x8070605], 28)))
+  t.equal('0x1080706050403020', HEX64(ROTR64([0x4030201, 0x8070605], 4)))
+  t.equal('0x0403020108070605', HEX64(ROTR64([0x4030201, 0x8070605], 32)))
+  t.equal('0x8070605040302010', HEX64(ROTR64([0x4030201, 0x8070605], 60)))
+  t.equal('0x7772fc2886a76c5f', HEX64(ROTR64([0x6c5f7772, 0xfc2886a7], 16)))
+  t.end()
+})
 
-function testXOR64() {
-  assertEquals('0x0000000600000002', HEX64(XOR64([1, 2], [3, 4])))
-  assertEquals('0x1234567812121212', HEX64(XOR64([0x10101010, 0x12005600], [0x02020202, 0x00340078])))
-}
+test('ADD64', function (t) {
+  t.equal('0x0000000200000000', HEX64(ADD64([0xffffffff, 1], [1, 0])))
+  t.equal('0x00000002fffffffe', HEX64(ADD64([0xffffffff, 1], [0xffffffff, 0])))
+  t.equal('0x76eb1310ddd333a0', HEX64(ADD64([0xF3BCC908, 0x6A09E667], [0xEA166A98, 0x0CE12CA8])))
+  t.equal('0x76eb1310ddd333a0', HEX64(ADD64([-205731576, 1779033703], [-367629672, 216083624])))
+  t.end()
+})
 
-function testBLAKE2B() {
+test('XOR64', function (t) {
+  t.equal('0x0000000600000002', HEX64(XOR64([1, 2], [3, 4])))
+  t.equal('0x1234567812121212', HEX64(XOR64([0x10101010, 0x12005600], [0x02020202, 0x00340078])))
+  t.end()
+})
+
+test('BLAKE2b', function (t) {
   // From the example computation in the RFC
-  assertEquals('ba80a53f981c4d0d6a2797b69f12f6e94c212f14685ac4b74b12bb6fdbffa2d1' +
-    '7d87c5392aab792dc252d5de4533cc9518d38aa8dbf1925ab92386edd4009923',
+  t.equal('ba80a53f981c4d0d6a2797b69f12f6e94c212f14685ac4b74b12bb6fdbffa2d1' +
+  '7d87c5392aab792dc252d5de4533cc9518d38aa8dbf1925ab92386edd4009923',
     blake2bHex('abc'))
-  assertEquals('ba80a53f981c4d0d6a2797b69f12f6e94c212f14685ac4b74b12bb6fdbffa2d1' +
-    '7d87c5392aab792dc252d5de4533cc9518d38aa8dbf1925ab92386edd4009923',
-    blake2bHex(new Uint8Array([97,98,99])))
+  t.equal('ba80a53f981c4d0d6a2797b69f12f6e94c212f14685ac4b74b12bb6fdbffa2d1' +
+  '7d87c5392aab792dc252d5de4533cc9518d38aa8dbf1925ab92386edd4009923',
+    blake2bHex(new Uint8Array([97, 98, 99])))
 
-  assertEquals('786a02f742015903c6c6fd852552d272912f4740e15847618a86e217f71f5419' +
-   'd25e1031afee585313896444934eb04b903a685b1448b755d56f701afe9be2ce',
+  t.equal('786a02f742015903c6c6fd852552d272912f4740e15847618a86e217f71f5419' +
+  'd25e1031afee585313896444934eb04b903a685b1448b755d56f701afe9be2ce',
     blake2bHex(''))
 
-  assertEquals('a8add4bdddfd93e4877d2746e62817b116364a1fa7bc148d95090bc7333b3673' +
-   'f82401cf7aa2e4cb1ecd90296e3f14cb5413f8ed77be73045b13914cdcd6a918',
+  t.equal('a8add4bdddfd93e4877d2746e62817b116364a1fa7bc148d95090bc7333b3673' +
+  'f82401cf7aa2e4cb1ecd90296e3f14cb5413f8ed77be73045b13914cdcd6a918',
     blake2bHex('The quick brown fox jumps over the lazy dog'))
+
+  t.end()
+})
+
+// Prints out a uint64 represented as an array of two uint32s
+// Examples:
+//  HEX64([0x4030201, 0x8070605]) = '0x0807060504030201'
+//  HEX64(ROT64(0x4030201, 0x8070605, 16)) = '0x0201080706050403'
+function HEX64 (arr) {
+  var s0 = (arr[0] >= 0 ? arr[0] : 0x100000000 + arr[0]).toString(16)
+  var s1 = (arr[1] >= 0 ? arr[1] : 0x100000000 + arr[1]).toString(16)
+  return '0x' +
+    new Array(8 - s1.length + 1).join('0') + s1 +
+    new Array(8 - s0.length + 1).join('0') + s0
 }
-
-function assertEquals(expected, actual) {
-  if (expected !== actual) {
-    throw new Error('Expected ' + expected + ' found ' + actual)
-  }
-}
-
-function main() {
-  var numPassed = 0, numFailed = 0
-  for (var name in this) {
-    if (typeof(this[name]) !== 'function') {
-      continue
-    }
-    if (!this[name].name.startsWith('test')) {
-      continue
-    }
-    console.log('\nTESTING ' + this[name].name)
-
-    try {
-      this[name]()
-      numPassed++
-    } catch (e) {
-      console.error(e)
-      numFailed++
-    }
-  }
-  if (numFailed === 0) {
-    console.log('\n' + numPassed + ' tests run, all passed!')
-    process.exit(0)
-  } else {
-    console.log('\n' + numFailed + '/' + (numPassed + numFailed) + ' tests failed')
-    process.exit(1)
-  }
-}
-
-main()
