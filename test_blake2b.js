f3a4e14325ea35c7a1f2deab5aaa8bfbdec1f4b5
DC <dcposch@dcpos.ch>
2016-01-10 00:40:30 -0800

    Added more test vectors

--- test_blake2b.js
+++ test_blake2b.js
@@ -29,6 +29,14 @@ function testBLAKE2B() {
   assertEquals('ba80a53f981c4d0d6a2797b69f12f6e94c212f14685ac4b74b12bb6fdbffa2d1' +
     '7d87c5392aab792dc252d5de4533cc9518d38aa8dbf1925ab92386edd4009923',
     blake2bHex(new Uint8Array([97,98,99])))
+
+  assertEquals('786a02f742015903c6c6fd852552d272912f4740e15847618a86e217f71f5419' +
+   'd25e1031afee585313896444934eb04b903a685b1448b755d56f701afe9be2ce',
+    blake2bHex(''))
+
+  assertEquals('a8add4bdddfd93e4877d2746e62817b116364a1fa7bc148d95090bc7333b3673' +
+   'f82401cf7aa2e4cb1ecd90296e3f14cb5413f8ed77be73045b13914cdcd6a918',
+    blake2bHex('The quick brown fox jumps over the lazy dog'))
 }
 
 function assertEquals(expected, actual) {
