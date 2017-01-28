a99222087e73449b3fc64160c2d70024f9b73938
DC <dcposch@dcpos.ch>
2017-01-27 19:40:43 -0800

    remove Object.assign, fix #1

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 var b2b = require('./blake2b')
 var b2s = require('./blake2s')
-module.exports = Object.assign({}, b2b, b2s)
+
+module.exports = {
+  blake2b: b2b.blake2b,
+  blake2bHex: b2b.blake2bHex,
+  blake2b_init: b2b.blake2b_init,
+  blake2b_update: b2b.blake2b_update,
+  blake2b_final: b2b.blake2b_final,
+  blake2s: b2s.blake2s,
+  blake2sHex: b2s.blake2sHex,
+  blake2s_init: b2s.blake2s_init,
+  blake2s_update: b2s.blake2s_update,
+  blake2s_final: b2s.blake2s_final
+}
